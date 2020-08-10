8b54c5fcd5426cf489914fd9da9831e5fb6aff9b
Jonas <jonwow@pm.me>
2020-08-10 20:47:22 +0300

    initial

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -5,9 +5,28 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
+        <div>
+          <img src={process.env.PUBLIC_URL + '/images/icons/slaveryICON_internetDL.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/tacICON_demo.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/careerICON.png'} />
+        </div>
+
+
+        <div>
+
+        </div>
+
+
+        <div>
+          <img src={process.env.PUBLIC_URL + '/images/icons/slaveryICON_internetDL.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/tacICON_demo.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/careerICON.png'} />
+        </div>
+
+
         <p>MB „PLASTIC FUTURE" © 2020, all rights reserved.</p>
       </footer>
     );
-    
+
   }
 }
