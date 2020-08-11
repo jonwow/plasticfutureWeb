4d5d66739125c4856a87fbf56b3c6ddd45901430
Jonas <jonwow@pm.me>
2020-08-11 20:58:56 +0300

    small changes in the footer

--- src/components/footer.component.js
+++ src/components/footer.component.js
@@ -5,21 +5,19 @@ export default class Footer extends Component {
   render() {
     return (
       <footer>
-        <div>
-          <img src={process.env.PUBLIC_URL + '/images/icons/slavery_resized.png'} />
-          <img src={process.env.PUBLIC_URL + '/images/icons/tac2_resized.png'} />
-          <img src={process.env.PUBLIC_URL + '/images/icons/career_resized.png'} />
-        </div>
 
         <div>
           <p>MB „PLASTIC FUTURE" © 2020, all rights reserved.</p>
         </div>
 
 
-        <div>
+        <div class="imageContainer">
           <img src={process.env.PUBLIC_URL + '/images/icons/instagram_icon.png'} />
           <img src={process.env.PUBLIC_URL + '/images/icons/twitter_icon.png'} />
           <img src={process.env.PUBLIC_URL + '/images/icons/youtube_icon.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/slavery_resized.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/tac2_resized.png'} />
+          <img src={process.env.PUBLIC_URL + '/images/icons/career_resized.png'} />
         </div>
 
 
