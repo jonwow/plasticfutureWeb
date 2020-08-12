eb4397504545aa607af495df0a68204e31ad17d6
Jonas <jonwow@pm.me>
2020-08-12 23:05:22 +0300

    removed unnecessary css

--- src/App.js
+++ src/App.js
@@ -17,12 +17,8 @@ function App() {
       <ScrollToTop />
 
       <Head />
-      {/* <Navbarx /> */}
-      <Navbar>
-        <NavItem icon="=">
-          <DropdownMenu/>
-        </NavItem>
-      </Navbar>
+     
+      <Navbarx />
       <div className="container">
         <Route path="/" exact component={ProductList} />
         <Route path="/products/:id/:color/" exact component={ProductPage} />
@@ -40,63 +36,6 @@ function App() {
   );
 }
 
-function Navbar(props) {
-  return (
-    <nav className="navbar">
-      <ul className="navbar-nav">{props.children}</ul>
-    </nav>
-  );
-}
 
-function NavItem(props) {
-  // call the useState function (hook)
-  // open = bool for current state.(whether the dropdown menu is open)
-  // setOpen = function that 'sets state action'
-  // false because the dropdown is not 'clicked' by default
-  const [open, setOpen] = useState(false);
-  return (
-    <li className="nav-item">
-      <p  className="icon-button" onClick={() => setOpen(!open)}>
-        {props.icon}
-      </p>
-
-      {open && props.children}
-      
-    </li>
-  );
-}
-
-// need more dropdown levels? 11:40 https://www.youtube.com/watch?v=IF6k0uZuypA
-function DropdownMenu() {
-  function DropdownItem(props) {
-    return <p className="menu-item" >{props.children}</p>;
-  }
-
-  return (
-    // change to state rendering instead of a href asap
-    <div className="dropdown">
-      <DropdownItem>
-        <a href="/tshirts">TSHIRTS</a>
-      </DropdownItem>
-
-      <DropdownItem>
-        <a href="/toteBags">TOTE BAGS</a>
-      </DropdownItem>
-
-      <DropdownItem>
-        <a href="/yourAccount">YOUR ACCOUNT</a>
-      </DropdownItem>
-
-      <DropdownItem>
-        <a href="/collections/">COLLECTIONS</a>
-      </DropdownItem>
-
-      <DropdownItem>
-        <a href="/contacts">CONTACTS</a>
-      </DropdownItem>
-
-    </div>
-  );
-}
 
 export default App;
--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -1,30 +1,21 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
   render() {
-    
+
 
     return (
+
+
       <navbar>
         <div className="navbarOne">
           <div className="centeringParent">
-            <div className="dropdown">
-              <div>
-                <img
-                  src={`${process.env.PUBLIC_URL}/images/navbar/threeLines.png`}
-                  alt="threeLines"
-                  className="clickable"
-                ></img>
-                <div className="dropdown-content">
-                  <a>T-SHIRTS</a>
-                  <a>TOTE BAGS</a>
-                  <a>YOUR ACCOUNT</a>
-                  <a>COLLECTIONS</a>
-                  <a>CONTACTS</a>
-                </div>
-              </div>
-            </div>
+            <threeLines style={{position: "relative", left: "50%", width: "5vh", listStyle: "none"}}>
+              <NavItem icon="=">
+                <DropdownMenu />
+              </NavItem>
+            </threeLines>
           </div>
         </div>
 
@@ -32,7 +23,7 @@ export default class Navbar extends Component {
           <div className="centeringParent" id="navbarText">
             <Link to="/">PLASTIC FUTURE
              </Link>
-             
+
           </div>
         </div>
         <div className="navbarThree">
@@ -44,4 +35,67 @@ export default class Navbar extends Component {
     );
 
   }
+}
+
+
+
+
+
+function threeLines(props) {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">{props.children}</ul>
+    </nav>
+  );
+}
+
+function NavItem(props) {
+  // call the useState function (hook)
+  // open = bool for current state.(whether the dropdown menu is open)
+  // setOpen = function that 'sets state action'
+  // false because the dropdown is not 'clicked' by default
+  const [open, setOpen] = useState(false);
+  return (
+    <li className="nav-item">
+      <p className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </p>
+
+      {open && props.children}
+
+    </li>
+  );
+}
+
+// need more dropdown levels? 11:40 https://www.youtube.com/watch?v=IF6k0uZuypA
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return <p className="menu-item" >{props.children}</p>;
+  }
+
+  return (
+    // change to state rendering instead of a href asap
+    <div className="dropdown">
+      <DropdownItem>
+        <a href="/tshirts">TSHIRTS</a>
+      </DropdownItem>
+
+      <DropdownItem>
+        <a href="/toteBags">TOTE BAGS</a>
+      </DropdownItem>
+
+      <DropdownItem>
+        <a href="/yourAccount">YOUR ACCOUNT</a>
+      </DropdownItem>
+
+      <DropdownItem>
+        <a href="/collections/">COLLECTIONS</a>
+      </DropdownItem>
+
+      <DropdownItem>
+        <a href="/contacts">CONTACTS</a>
+      </DropdownItem>
+
+    </div>
+  );
 }
