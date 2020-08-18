1287d0ce06c4bd68d812892e373d6d38a447e61c
Jonas <jonwow@pm.me>
2020-08-18 22:04:52 +0300

    progress towards sorting of products. commit reason - need a semi stable 'before' files

--- src/components/product-list.component.js
+++ src/components/product-list.component.js
@@ -58,42 +58,46 @@ export default class ProductList extends Component {
     }
 
     productList(productType) {
-        var currentStyle = {
-            
-        };
-
         // for every product
-        return this.state.products.map(curProduct => {
+        var array = ''
+        var masyvas = []
+        console.log(this.state.products)
+        var i = 0
+        this.state.products.map(curProduct => {
+            var currentStyle = {};
+
             if (curProduct.public) {
                 // if NOT in stock or ready to be sold
-                if (!curProduct.available) {
-                    currentStyle = {
-                        filter: "grayscale(1) blur(1px)"
-                    }
-                }
-                else {
-                    currentStyle = {
-                    }
-                }
-                // example: plasticfuture.net/products/jeans will only display jeans type products
-                if (curProduct.type == productType) {
-                    return curProduct.color.map(curColor => {
-                        // gives 'props.color' and 'props.product' to the Product const
-                        return <Product style={currentStyle} color={curColor} product={curProduct} />;
-                    })
-                }
-                // if no productType is provided, it will display all products
-                else if (productType == undefined) {
-                    return curProduct.color.map(curColor => {
-                        // gives 'props.color' and 'props.product' to the Product const
-                        return <Product style={currentStyle} color={curColor} product={curProduct} />;
-                    })
-                }
+                if (!curProduct.available)
+                    currentStyle = { filter: "grayscale(1) blur(1px)" }
 
+                // example: plasticfuture.net/products/jeans will only display jeans type products
+                // if no productType is provided, it will display all products
+                if (curProduct.type == productType || productType == undefined)
+                    curProduct.color.map(curColor => {
+                        // gives 'props.color' and 'props.product' to the Product const
+                        array += i
+                        console.log(curProduct.name)
+                        console.log(' vis dar kepu')
+                    })
             }
+            console.log(array + '\n----------------------------\n')
+
+            i++
+            console.log('i yra ' + i)
 
 
         })
+
+        // add colors to this and then start implementing the sorting
+        for (var x = 0; x < array.length; x++)
+            masyvas.push(this.state.products[array[x]])
+
+
+        return masyvas.forEach(element => {
+            return 22 
+        });
+
     }
 
 
@@ -113,7 +117,7 @@ export default class ProductList extends Component {
                     {
                         this.state.loading ?
 
-                            <p style={{ textAlign: 'center', fontSize: '100px', margin: '110px 0', paddingBottom: '370px' }}></p>
+                            <p style={{ textAlign: 'center', fontSize: '100px', margin: '110px 0', paddingBottom: '100vh' }}></p>
                             :
                             this.productList(productType)
                     }
