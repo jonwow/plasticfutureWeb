d6054ad5b3ff6e3b4106014a453cb1c50ef26b63
Jonas <jonwow@pm.me>
2020-11-19 10:12:09 +0200

    bugfix:ENTER WEB > PRESS ON PRODUCT> CLICK PURCHASE W/O SELECTING A SIZE>SELECT A SIZE> PURCHASE> GO BACK, SCRLL

--- src/App.js
+++ src/App.js
@@ -26,10 +26,10 @@ const SearchableList = () => {
       document.getElementById("choose-a-size-msg").style.cssText= " transition: filter 0.75s; color: white; display: inline; z-index: 999; position: absolute; top: 35%; left: 42vw; background: rgba(0, 0, 0, 0.85); padding: 4rem; border: 1px solid black; font-size: 30px";
 
       setTimeout(() => {
-        document.getElementsByClassName("container")[0].style.cssText = "transition: 0.75s; filter: blur(0px) grayscale(0)";
+        document.getElementsByClassName("container")[0].style.cssText = "transition: 0.75s; ";
      document.getElementsByTagName("nav")[0].style.cssText = "transition: 0.75s; filter: blur(0px) grayscale(0)";
       document.getElementById("choose-a-size-msg").style.cssText= "display: none; transition: all 0.75s; z-index: 999; position: absolute; top: 50%; left: 47vw; background:white; padding: 2rem; border: 1px solid black";
-      }, 4000);
+      }, 2700);
     }
     else {
       var unique = true;
